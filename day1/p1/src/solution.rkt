76ff196cefd24f26b305ff75b09f5978e497609c
Daniel Miller <danm1113@gmail.com>
2018-12-02 20:26:49 -0800

    Day 1, Problem 1 - better solution

--- day1/p1/src/solution.rkt
+++ day1/p1/src/solution.rkt
@@ -3,9 +3,7 @@
 (define path "../data/frequencies")
 (define input (open-input-file path #:mode 'text))
 (define input-lines (port->lines input))
-(define sum (lambda
-	(a b) (+ a b)))
 
 (printf "~a" (foldl
-	sum 0 (map
+	+ 0 (map
 		string->number input-lines)))
