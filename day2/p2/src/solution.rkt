a28cf87f14d9019cc400cbc5b7be2eea78012a54
Daniel Miller <danm1113@gmail.com>
2018-12-11 14:17:42 -0800

    d2p2 - answer is correct, but not how advent wanted it. Don't ask for common characters if you won't accept a set of common characters.

--- day2/p2/src/solution.rkt
+++ day2/p2/src/solution.rkt
@@ -1,6 +1,18 @@
 #lang racket
 
-(provide word->regex lift2 ap test-list)
+(provide word->regex lift2 ap test-list regex-list input-match)
+
+(define
+  file-list
+  (port->lines (open-input-file "../../data/boxes" #:mode 'text)))
+
+(define
+  no-false
+  ((curry filter)
+   (lambda
+     (i)
+     (not (eq? i false)))))
+
 
 (define
   test-list
@@ -11,6 +23,7 @@
     "fguij"
     "axcye"
     "wvxyz"))
+
 (define form-regex (lambda
   [word length]
   (lambda
@@ -23,7 +36,7 @@
 ;; forall f. f (a -> b) -> f a -> f b
 (define ap (lambda
   [bfn fx]
-  (foldl
+  (foldr
     (lambda
       [x acc]
         (append
@@ -53,4 +66,61 @@
         length
         (form-regex word length))))))
 
-;; (lift2 regexp-match (map word->regex test-list) test-list)
+(define regex-list (foldl
+  (lambda
+    (word acc)
+    (list*
+      (cons
+	(word->regex word)
+	word)
+      acc))
+  '()
+  test-list))
+
+(define regex-list2 (foldl
+  (lambda
+    (word acc)
+    (list*
+      (cons
+	(word->regex word)
+	word)
+      acc))
+  '()
+  file-list))
+
+(define match-word
+  (lambda
+    (item test-word)
+    (let
+      ([word (cdr item)]
+       [regex (car item)])
+      (if
+	(eq? test-word word)
+	false
+	(foldl
+	  (lambda
+	    (reg acc)
+	    (if
+	      (regexp-match reg test-word)
+	      word
+	      acc))
+	  false
+	  regex)))))
+
+(define
+  common-letters
+  (lambda
+    (word1 word2)
+    (let*
+      ([letters1 (apply set (string->list word1))]
+       [letters2 (apply set (string->list word2))])
+      (apply string (set->list (set-intersect letters1 letters2))))))
+
+(define test-match (no-false (lift2 match-word regex-list test-list)))
+
+(define
+  input-match
+  (no-false (lift2 match-word regex-list2 file-list)))
+
+(common-letters (car input-match) (cadr input-match))
+(common-letters (car test-match) (cadr test-match))
