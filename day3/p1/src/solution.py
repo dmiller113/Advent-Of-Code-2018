f3aaec2ad26aaca92e5010936c70d12387e96ec2
Daniel Miller <danm1113@gmail.com>
2018-12-13 19:05:37 -0800

    solution - up to sorted coords.

--- day3/p1/src/solution.py
+++ day3/p1/src/solution.py
@@ -2,6 +2,8 @@
 
 import re
 from functools import reduce
+from itertools import chain
+from operator import itemgetter
 
 plan_regex = r"#(.*) @ (.*),(.*): (.*)x(.*)"
 plan_id = { "name": "id", "index": 1 }
@@ -17,18 +19,76 @@ def compose(*functions):
 	return inner
 
 
-def plan_to_match(plan_string):
+def string_to_match(plan_string):
 	return re.match(plan_regex, plan_string)
 
-def match_to_coord(match):
+def match_to_plan(match):
+	def reduce_fn(acc, x):
+		key = x.get("name")
+		val = match.group(x.get("index"))
+		if not key == "id":
+			val = int(val)
+
+		return dict(acc, **{ key: val})
+
 	return reduce(
-		lambda acc,x: dict(acc, **{ x.get("name"): match.group(x.get("index"))}),
-			[plan_id, plan_x, plan_y, plan_w, plan_h],
-			{}
+		reduce_fn,
+		[plan_id, plan_x, plan_y, plan_w, plan_h],
+		{}
 	)
 
-plan_to_coord = compose(plan_to_match, match_to_coord)
+string_to_plan = compose(string_to_match, match_to_plan)
 
-with open("../../data/plans") as f:
-	for line in f:
-		print(line)
+def plan_to_coords(plan):
+	def map_fn(i):
+		dx = i % plan["w"]
+		dy = i // plan["w"]
+		return (plan["x"] + dx, plan["y"] + dy)
+
+	total_squares = range(0, plan["w"] * plan["h"])
+	return map(map_fn, total_squares)
+
+string_to_coords = compose(string_to_plan, plan_to_coords)
+
+def sort_coord_gen(*args):
+	return sorted(
+		chain(*args),
+		key=itemgetter(1,0),
+	)
+
+
+
+#########
+# Tests #
+#########
+
+
+def test_single_string():
+	test_string = "#123 @ 3,2: 5x4"
+	coords_gen = plan_to_coords(string_to_plan(test_string))
+
+	for coord in coords_gen:
+		print(coord)
+
+def test_solution():
+	with open("../../data/plans") as f:
+		plans = map(string_to_plan, f)
+		coords = map(plan_to_coords, plans)
+		for coord in coords:
+			for c in coord:
+				print(c)
+
+def test_overlap():
+	string1 = "#1 @ 1,3: 4x4"
+	string2 = "#2 @ 3,1: 4x4"
+	string3 = "#1 @ 5,5: 2x2"
+	coords = sort_coord_gen(
+		string_to_coords(string1),
+		string_to_coords(string2),
+		string_to_coords(string3),
+	)
+
+	for c in coords:
+		print(c)
+
+test_overlap()
